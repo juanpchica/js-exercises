d935794ee7d009abe0745bfaa3618b89bdaa93b6
juanpchica <juanpchica@hotmail.com>
2021-05-28 14:07:07 -0500

    Map and foreach difference

--- test2.js
+++ test2.js
@@ -2,10 +2,30 @@ const reverseString = (str) => {
   //console.log(str.split("").reverse().join(""));
 
   let revString = "";
-  for (let letter of str) {
-    revString = letter + revString;
-  }
+  //   for (let letter of str) {
+  //     revString = letter + revString;
+  //   }
 
-  return revString;
+  //str.split("").forEach((el) => (revString = el + revString));
+
+  return str
+    .split("")
+    .reduce((accumulator, currentValue) => currentValue + accumulator); //=);
 };
 reverseString("hello juan"); //=
+
+const doubleNumber = (numbers) => {
+  // numbers.forEach((num,index) => {
+  //     return  num*2; //=
+  // });
+
+  // return numbers //=
+
+  return numbers.map((n) => {
+    return n * 2; //=
+  });
+
+  numbers;
+};
+
+doubleNumber([2, 5, 6, 7]); //=
