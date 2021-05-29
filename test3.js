99d52520d11c8fe24160f298017454c876aaaad8
juanpchica <juanpchica@hotmail.com>
2021-05-29 16:49:38 -0500

    MerchantSocks exercise

--- test3.js
+++ test3.js
@@ -1,5 +1,5 @@
 let p = new Promise((resolve, reject) => {
-  let a = 1 + 2;
+  let a = 1 + 1;
   if (a == 2) {
     resolve("Success");
   } else {
@@ -12,3 +12,21 @@ p.then((message) => {
 }).catch((message) => {
   console.log("this is in the catch " + message);
 });
+
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("greeting");
+  }, 3000);
+});
+
+myPromise.then((resp) => {
+  console.log(resp); //=
+});
+
+const greetings = async (name) => {
+  const greet = await myPromise(); //=
+
+  console.log(`${greet} ${name}`); //=
+};
+
+greetings("Juan"); //=
---
+++ test4.js
@@ -0,0 +1,53 @@
+/*
+There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+Example
+
+
+There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+
+Function Description
+
+Complete the sockMerchant function in the editor below.
+
+sockMerchant has the following parameter(s):
+
+int n: the number of socks in the pile
+int ar[n]: the colors of each sock
+Returns
+
+int: the number of pairs
+Input Format
+
+The first line contains an integer , the number of socks represented in .
+The second line contains  space-separated integers, , the colors of the socks in the pile.
+*/
+function sockMerchan(n, ar) {
+  //Validate constrains
+  if (!ar && ar.lenght !== n) return 0;
+
+  let totalPairs = 0;
+  let mapPairs = {};
+
+  //Calculate numbers of pairs
+  ar.forEach((sock) => {
+    if (!mapPairs[sock]) {
+      mapPairs[sock] = 1;
+    } else {
+      mapPairs[sock]++;
+    }
+  });
+
+  for (const colorSock in mapPairs) {
+    if (mapPairs[colorSock] % 2 === 0) {
+      totalPairs += mapPairs[colorSock] / 2; //=
+    } else {
+      totalPairs += (mapPairs[colorSock] - 1) / 2;
+    }
+  }
+
+  //Return number of pairs
+  return totalPairs;
+}
+
+sockMerchan(9, [1, 3, 4, 1, 1, 3, 4, 4, 5]); //=
