169f69075cdba5cc2b56c4c86acc40f3507d197b
juanpchica <juanpchica@hotmail.com>
2021-08-01 17:20:10 -0500

    Creating object to have multiple instances

--- test8.js
+++ test8.js
@@ -1,11 +1,26 @@
-const Book = {
-  title: "The Hobbit",
-  author: "Juan P Chica",
-  year: 2021,
-  showInfo: function () {
-    return "works " + this.title;
-  },
-};
+// const Book = {
+//   title: "The Hobbit",
+//   author: "Juan P Chica",
+//   year: 2021,
+//   showInfo: function () {
+//     return "works " + this.title;
+//   },
+// };
 
-Book.title; //=
-Book.showInfo(); //=
+// Book.title; //=
+// Book.showInfo(); //=
+
+function Book(title, year, author) {
+  this.title = title;
+  this.year = year;
+  this.author = author;
+  this.showInfo = function () {
+    return `${this.title} + ${this.author}`;
+  };
+}
+
+const book1 = new Book("The Hobbit", 2015, "Juan Pablo Chica");
+book1.showInfo(); //=
+
+const book2 = new Book("The Lord Of the Rings", 2015, "Nico Chica");
+book2.showInfo(); //=
