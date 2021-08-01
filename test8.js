bd7e93ea889a4122fdb95a609f08c6a86553bb94
juanpchica <juanpchica@hotmail.com>
2021-08-01 17:12:27 -0500

    Book object with basic propierties

---
+++ test8.js
@@ -0,0 +1,11 @@
+const Book = {
+  title: "The Hobbit",
+  author: "Juan P Chica",
+  year: 2021,
+  showInfo: function () {
+    return "works " + this.title;
+  },
+};
+
+Book.title; //=
+Book.showInfo(); //=
