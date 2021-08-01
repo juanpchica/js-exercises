4aee6f9e60895473f2f9391f3284f6581ebdf88b
juanpchica <juanpchica@hotmail.com>
2021-08-01 15:37:47 -0500

    Adding for in a normal function loop

--- test2.js
+++ test2.js
@@ -158,3 +158,11 @@ const companies = [
 ];
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+function normalLoop() {
+  for (let i = 0; i < companies.length; i++) {
+    console.log(companies[i]); //=
+  }
+}
+
+normalLoop();
